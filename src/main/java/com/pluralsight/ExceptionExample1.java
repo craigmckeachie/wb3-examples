a37c89e84db54af3ffad262bd1f1e25c629dfe14
Craig McKeachie <craigdmckeachie@gmail.com>
2026-04-20 14:02:05 -0400

    Example: ExceptionExample1

---
+++ src/main/java/com/pluralsight/ExceptionExample1.java
@@ -0,0 +1,20 @@
+package com.pluralsight;
+
+public class ExceptionExample1 {
+
+    public static void main(String[] args) {
+        String[] names = {
+                "Ezra", "Elisha", "Ian",
+                "Siddalee", "Pursalane", "Zephaniah"
+        };
+
+        try {
+            String name = names[6];
+            System.out.println(name);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+
+    }
+}
